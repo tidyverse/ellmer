8b41d7de146de127b034fec0544896b5265572be
Hadley Wickham <h.wickham@gmail.com>
2025-01-22 17:57:23 -0600

    WIP: add open router backend
    Fixes #212

---
+++ tests/testthat/test-provider-openrouter.R
@@ -0,0 +1,45 @@
+# Getting started --------------------------------------------------------
+
+test_that("can make simple request", {
+  chat <- chat_open_router("Be as terse as possible; no punctuation", model = "gpt-4o-mini")
+  resp <- chat$chat("What is 1 + 1?", echo = FALSE)
+  expect_match(resp, "2")
+  expect_true(all(chat$last_turn()@tokens >= 1))
+})
+
+test_that("can make simple streaming request", {
+  chat <- chat_open_router("Be as terse as possible; no punctuation", model = "gpt-4o-mini")
+  resp <- coro::collect(chat$stream("What is 1 + 1?"))
+  expect_match(paste0(unlist(resp), collapse = ""), "2")
+})
+
+# Common provider interface -----------------------------------------------
+
+test_that("respects turns interface", {
+  chat_fun <- function(...) chat_open_router(..., model = "gpt-4o-mini")
+
+  test_turns_system(chat_fun)
+  test_turns_existing(chat_fun)
+})
+
+test_that("all tool variations work", {
+  chat_fun <- function(...) chat_open_router(..., model = "gpt-4o-mini")
+
+  test_tools_simple(chat_fun)
+  test_tools_async(chat_fun)
+  test_tools_parallel(chat_fun)
+  test_tools_sequential(chat_fun, total_calls = 6)
+})
+
+test_that("can extract data", {
+  chat_fun <- function(...) chat_open_router(..., model = "gpt-4o-mini")
+
+  test_data_extraction(chat_fun)
+})
+
+test_that("can use images", {
+  chat_fun <- function(...) chat_open_router(..., model = "gpt-4o-mini")
+
+  test_images_inline(chat_fun)
+  test_images_remote(chat_fun)
+})
