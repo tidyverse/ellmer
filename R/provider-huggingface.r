eb6cb50c5a88986e5adaacf22fc9e5973e8b5b18
Simon Spavound <simon.spavound@googlemail.com>
2025-03-07 11:19:41 -0500

    this commit adds a new wrapper around openai to provide support to hugging face serverless endpoints

---
+++ R/provider-huggingface.r
@@ -0,0 +1,58 @@
+#' Chat with a model hosted on Hugging Face Serverless Inference API
+#'
+#' @description
+#' [Hugging Face](https://huggingface.co/) hosts a variety of open-source
+#' and proprietary AI models available via their Inference API.
+#' To use the Hugging Face API, you must have an Access Token, which you can obtain
+#' from your [Hugging Face account](https://huggingface.co/settings/tokens).
+#'
+#' This function is a lightweight wrapper around [chat_openai()], with
+#' the defaults adjusted for Hugging Face. Model defaults to `meta-llama/Llama-3.1-8B-Instruct`.
+#'
+#' ## Known limitations
+#'
+#' * Some models do not support the chat interface or parts of it, for example
+#' `google/gemma-2-2b-it` does not support a system prompt. You will need to
+#'  carefully choose the model.
+#'
+#' @family chatbots
+#' @param api_key The API key to use for authentication. You should not
+#'   supply this directly; instead, store your Hugging Face API key as an
+#'   environment variable (`HUGGINGFACE_API_KEY`) in your `.Renviron` file.
+#'   Use `usethis::edit_r_environ()` to modify it.
+#' @export
+#' @inheritParams chat_openai
+#' @inherit chat_openai return
+#' @examples
+#' \dontrun{
+#' chat <- chat_hf()
+#' chat$chat("Tell me three jokes about statisticians")
+#' }
+chat_hf <- function(system_prompt = NULL,
+                    turns = NULL,
+                    base_url = "https://api-inference.huggingface.co/models/",
+                    api_key = hf_key(),
+                    model = NULL,
+                    seed = NULL,
+                    api_args = list(),
+                    echo = NULL) {
+
+  model <- set_default(model, "meta-llama/Llama-3.1-8B-Instruct")
+  echo <- check_echo(echo)
+
+  chat_openai(
+    system_prompt = system_prompt,
+    turns = turns,
+    # modify base_url for hugging face compatability with openai
+    base_url = paste0(base_url, model, "/v1"),
+    api_key = api_key,
+    model = model,
+    seed = seed,
+    api_args = api_args,
+    echo = echo
+  )
+}
+
+hf_key <- function() {
+  key_get("HUGGINGFACE_API_KEY")
+}
