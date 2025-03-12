e4e00d16e057d90781a2546d4b412550ed59416a
Hadley Wickham <h.wickham@gmail.com>
2025-03-12 14:23:37 +1100

    Fix test

--- tests/testthat/test-chat.R
+++ tests/testthat/test-chat.R
@@ -158,10 +158,14 @@ test_that("can extract structured data (async)", {
   expect_equal(data, list(name = "John", age = 15))
 })
 
-test_that("can retrieve tokens", {
+test_that("can retrieve tokens with or without system prompt", {
   chat <- chat_openai("abc")
-  expect_true(nrow(chat$tokens(FALSE)), 0)
-  expect_true(nrow(chat$tokens(TRUE)), 1))
+  expect_equal(nrow(chat$tokens(FALSE)), 0)
+  expect_equal(nrow(chat$tokens(TRUE)), 1)
+
+  chat <- chat_openai()
+  expect_equal(nrow(chat$tokens(FALSE)), 0)
+  expect_equal(nrow(chat$tokens(TRUE)), 0)
 })
 
 test_that("has a basic print method", {
