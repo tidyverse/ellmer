3c9628dcb3deb4a830d95c4785a112592617ecf9
Hadley Wickham <h.wickham@gmail.com>
2025-05-08 12:57:07 -0500

    Apply suggestions from code review
    Co-authored-by: Garrick Aden-Buie <garrick@adenbuie.com>

--- R/chat-parallel.R
+++ R/chat-parallel.R
@@ -13,9 +13,9 @@
 #'   For [chat_anthropic()], note that the number of active connections is
 #'   limited primarily by the output tokens per limit (OTPM) which is estimated
 #'   from the `max_tokens` parameter, which defaults to 4096. That means if
-#'   your usage tier limits you to 16,000 OTPM, you should use either set to
-#'   `max_active = 4` (16,000 / 4096) to decrease the number of max tokens
-#'   with [params()].
+#'   your usage tier limits you to 16,000 OTPM, you should use either set
+#'   `max_active = 4` (16,000 / 4096) to decrease the number of active
+#'   connections or use [params()] in [chat_anthropic()] to decrease `max_tokens`.
 #' @param rpm Maximum number of requests per minute.
 #' @return A list of [Chat] objects, one for each prompt.
 #' @examplesIf ellmer::has_credentials("openai")
@@ -24,6 +24,8 @@
 #'
 #' chat <- chat_openai()
 #' parallel_chat(chat, prompts)
+#'
+#' @export
 parallel_chat <- function(chat, prompts, max_active = 10, rpm = 500) {
   my_parallel_responses <- function(conversations) {
     parallel_responses(
