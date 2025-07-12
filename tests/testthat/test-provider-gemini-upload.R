b5d7e3c1c77526d34c0faadc0f880421e1b3db7b
Hadley Wickham <h.wickham@gmail.com>
2025-07-12 08:14:38 -0500

    Start debugging the upload problem

--- tests/testthat/test-provider-gemini-upload.R
+++ tests/testthat/test-provider-gemini-upload.R
@@ -1,7 +1,7 @@
 test_that("can upload a file an ask questions about it", {
   upload <- google_upload(test_path("apples.pdf"))
 
-  chat <- chat_google_gemini()
+  chat <- chat_google_gemini_test()
   response <- chat$chat("What's the title of this document?", upload)
   expect_match(response, "Apples are tasty")
   expect_match(chat$chat("What apple is not tasty?"), "red delicious")
