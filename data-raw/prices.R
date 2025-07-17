d103bd048277527238ab48339d430d9cf911c11d
Hadley Wickham <h.wickham@gmail.com>
2025-07-17 08:10:47 -0500

    More providers; remove old standardise approach

--- data-raw/prices.R
+++ data-raw/prices.R
@@ -4,16 +4,8 @@ library(tidyr)
 litellm_url <- "https://raw.githubusercontent.com/BerriAI/litellm/refs/heads/main/model_prices_and_context_window.json"
 litellm_prices <- jsonlite::read_json(litellm_url)
 
-# fmt: skip
-provider_lookup <- tribble(
-  ~litellm_provider, ~provider, 
-  "openai", "OpenAI",
-  "anthropic", "Anthropic",
-  "gemini", "Google/Gemini"
-)
-
 df <- tibble::enframe(litellm_prices, "model", "data")
-prices <- df |>
+all_prices <- df |>
   hoist(
     data,
     provider = "litellm_provider",
@@ -31,13 +23,25 @@ prices <- df |>
   ) |>
   relocate(provider) |>
   arrange(provider, model) |>
-  filter(input > 0 | output > 0) |>
+  filter(input > 0 | output > 0)
+all_prices |> count(provider, sort = TRUE)
+
+# fmt: skip
+provider_lookup <- tribble(
+  ~litellm_provider, ~provider, 
+  "openai", "OpenAI",
+  "anthropic", "Anthropic",
+  "gemini", "Google/Gemini",
+  "azure", "Azure/OpenAI",
+  "bedrock", "AWS/Bedrock",
+  "mistral", "Mistral",
+)
+
+prices <- all_prices |>
   inner_join(provider_lookup, join_by(provider == litellm_provider)) |>
   mutate(provider = provider.y, provider.y = NULL)
 
-prices |>
-  View()
-
+# prices |> View()
 
 usethis::use_data(prices, overwrite = TRUE, internal = TRUE)
 
