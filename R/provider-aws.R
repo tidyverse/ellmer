57eb6e5a5133c5eb813679fdc4918b5637822a3e
Garrick Aden-Buie <garrick@adenbuie.com>
2025-09-03 11:42:22 -0400

    refactor(aws): Even more compact

--- R/provider-aws.R
+++ R/provider-aws.R
@@ -230,14 +230,12 @@ method(chat_request, ProviderAWSBedrock) <- function(
 
   # Merge params into inferenceConfig, giving precedence to manual api_args
   params <- chat_params(provider, provider@params)
-  extra_args <- provider@extra_args
 
-  if (length(params) > 0) {
-    extra_args$inferenceConfig <- modify_list(
-      params,
-      extra_args$inferenceConfig %||% list()
-    )
-  }
+  extra_args <- provider@extra_args
+  extra_args$inferenceConfig <- modify_list(
+    params,
+    extra_args$inferenceConfig %||% list()
+  )
 
   # https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_Converse.html
   body <- compact(list2(
