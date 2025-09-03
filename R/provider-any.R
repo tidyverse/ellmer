6258dc9357074a10abfe3ab2852506767b9be398
Garrick Aden-Buie <garrick@adenbuie.com>
2025-09-03 11:17:10 -0400

    chore(chat): Include `params` in required formals

--- R/provider-any.R
+++ R/provider-any.R
@@ -38,8 +38,8 @@ chat <- function(
     cli::cli_abort("Can't find provider {.code ellmer::{provider_name}()}.")
   }
 
-  # chat() only supports chat_ functions that use `model` and `system_prompt`
-  if (any(!c("model", "system_prompt") %in% fn_fmls_names(chat_fun))) {
+  required_params <- c("model", "system_prompt", "params")
+  if (any(!required_params %in% fn_fmls_names(chat_fun))) {
     cli::cli_abort(
       "{.fn ellmer::chat} does not support {.fn ellmer::{provider_name}}, please call it directly.",
     )
