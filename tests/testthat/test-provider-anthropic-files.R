0061a5dec2dd41b6c01bf5e94ca0b73a7f8aae6b
Hadley Wickham <h.wickham@gmail.com>
2025-11-07 11:57:48 -0600

    Rename `anthropic_file_` to `claude_`

--- tests/testthat/test-provider-anthropic-files.R
+++ tests/testthat/test-provider-anthropic-files.R
@@ -1,8 +1,8 @@
 test_that("end-to-end test of all functions", {
   vcr::local_cassette("anthropic-upload-file", record = "all")
 
-  upload <- anthropic_file_upload(test_path("apples.pdf"))
-  defer(anthropic_file_delete(upload@uri))
+  upload <- claude_file_upload(test_path("apples.pdf"))
+  defer(claude_file_delete(upload@uri))
 
   chat <- chat_anthropic_test(beta_headers = "files-api-2025-04-14")
   response <- chat$chat("What's the title of this document?", upload)
@@ -15,8 +15,8 @@ test_that("end-to-end test of all functions", {
 
   # Can't download uploaded files, but at least tests that request succeeds
   path <- withr::local_tempfile()
-  expect_error(anthropic_file_download(upload@uri, path), "not downloadable")
+  expect_error(claude_file_download(upload@uri, path), "not downloadable")
 
-  files <- anthropic_file_list()
+  files <- claude_file_list()
   expect_true("apples.pdf" %in% files$filename)
 })
