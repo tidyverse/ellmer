7c96d67f4e0ce0beb42e014fbd93b298b1aa19e6
kaipingyang <1260146556@qq.com>
2026-07-09 13:16:01 +0000

    style: air format the on_request_start invoke calls
    Wrap the c(private$.turns, list(user_turn)) argument to satisfy air's 80-col
    width (fixes the format-suggest CI check). No behaviour change.

--- R/chat.R
+++ R/chat.R
@@ -641,7 +641,10 @@ Chat <- R6::R6Class(
       )
 
       while (!is.null(user_turn)) {
-        private$callback_on_request_start$invoke(c(private$.turns, list(user_turn)))
+        private$callback_on_request_start$invoke(c(
+          private$.turns,
+          list(user_turn)
+        ))
         assistant_chunks <- private$submit_turns(
           user_turn,
           stream = stream,
@@ -718,7 +721,10 @@ Chat <- R6::R6Class(
       )
 
       while (!is.null(user_turn)) {
-        await(private$callback_on_request_start$invoke_async(c(private$.turns, list(user_turn))))
+        await(private$callback_on_request_start$invoke_async(c(
+          private$.turns,
+          list(user_turn)
+        )))
         assistant_chunks <- private$submit_turns_async(
           user_turn,
           stream = stream,
